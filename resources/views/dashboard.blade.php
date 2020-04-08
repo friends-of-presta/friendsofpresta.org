e85335f969348725baebe022bd6c8b45687ab233
Yneet <contact@yneet.com>
2020-04-08 11:13:14 +0200

    fix dashboard sum

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,8 +18,8 @@
                         <div class="d-flex align-items-center justify-content-center">
                             <div class="display-2 text-center mr-2">{{ array_sum($inscription) }}</div>
                             <div>
-                                <div>En attente : <strong><a href="{{ route('admin.inscriptions') . '?status=A+attribuer' }}">{{ $inscription['A attribuer'] }}</a></strong></div>
-                                <div>En cours : <strong>{{ $inscription['création en cours'] }}</strong></div>
+                                <div>En attente : <strong><a href="{{ route('admin.inscriptions') . '?status=A+attribuer' }}">{{ array_key_exists('A attribuer', $inscription) ? $inscription['A attribuer'] : '0' }}</a></strong></div>
+                                <div>En cours : <strong>{{ array_key_exists('création en cours', $inscription) ? $inscription['création en cours'] : '0' }}</strong></div>
                             </div>
                         </div>
                     </div>
