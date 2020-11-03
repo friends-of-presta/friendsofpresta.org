f3bbb05923f4279a6b18c388e57e6534b9c58a5e
Jean-François Viguier <jf.viguier@gmail.com>
2020-11-03 18:47:42 +0100

    Suppression hébergement gratuit

--- resources/views/pages/ecommerce-solidaire/index.blade.php
+++ resources/views/pages/ecommerce-solidaire/index.blade.php
@@ -329,10 +329,7 @@
             <div class="ml-sm-5">
                 <p>
                     <strong>Vous êtes propriétaire de votre site</strong> et pouvez l'utiliser autant que que vous le voulez.
-                    L’hébergement sera accessible pendant toute la durée du confinement. L’objectif est d’aider les
-                    TPE à passer ce cap du confinement mais n’est pas de fournir une concurrence déloyale aux
-                    prestataires. L’hébergement sera arrêté le 31 Aout 2020 (soit environ deux mois après le déconfinement complet) vous pourrez
-                    récupérer l’ensemble du site pour l’héberger où vous le souhaitez, il reste à vous.
+                    L’hébergement est un abonnement mensuel et peut être arrêté quand vous le souhaitez.
                 </p>
             </div>
 
