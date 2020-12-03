7f168eb02a0e62618cd0bcca1ea9ccdb5c2877cc
Mickaël Andrieu <mickael.andrieu@solvolabs.com>
2020-12-03 10:59:51 +0100

    Decode JSON output into Blade view

--- app/Http/Controllers/Pages/CommunityModulesController.php
+++ app/Http/Controllers/Pages/CommunityModulesController.php
@@ -11,7 +11,7 @@ class CommunityModulesController extends Controller
     {
         $modules = file_get_contents('https://spreadsheets.google.com/feeds/list/1nTVVCNTPO6bj9pjBcMHdV5dRLSBClIrCENyQduwOu1w/od6/public/values?alt=json');
         return view('pages/community-modules/index', [
-            'modules' => modules,
+            'modules' => json_decode(modules),
         ]);
     }
 }
